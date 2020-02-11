67369f58525ded82959c1459d5756b9f8a4bb170
Matthew Clark <Everdein@gmail.com>
2020-02-10 22:26:27 -0800

    Commented Methods
    Added description of project and commented methods. Still need to implement route url syntax conversion and exceptions for incorrect user input.

--- RegularExpressions/src/RouteFinder/Client.java
+++ RegularExpressions/src/RouteFinder/Client.java
@@ -1,12 +1,24 @@
+/**
+ * Matthew Clark
+ * CS320 Programming Languages
+ * Community Transit RouteFinder Project
+ */
+
 package RouteFinder;
 
 import javafx.util.Pair;
 import java.io.IOException;
-import java.util.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Client extends RouteFinder
 {
+    // Allows Client.java to access RouteFinger.java methods.
     public static RouteFinder routeFinder = new RouteFinder();
+
+    // Main.
     public static void main(String[] args) throws IOException
     {
         Scanner scanner = new Scanner(System.in);
@@ -17,33 +29,37 @@ public class Client extends RouteFinder
             System.out.print("Please enter a letter that your destination starts with: ");
             String destination = scanner.next();
             String url = "https://www.communitytransit.org/busservice/schedules/";
+
             // Get URL text.
-            String urlText = routeFinder.getUrlText(url);
+            String urlText = null;
+            urlText = routeFinder.getUrlText(url);
+
             // Create map to store destination and routes.
             Map hashMap = routeFinder.getBusRoutesUrls(urlText, destination.toUpperCase().charAt(0));
+
             // Collects users desired bus route.
             System.out.print("\nPlease enter a route ID as a string: ");
             String routeID = scanner.next();
             hashMap = routeFinder.getRouteStops(hashMap, routeID);
 
+            // Iterates through the map and prints to and from destination with stop numbers and stop names.
             Iterator<Map.Entry<String, List<Pair<String, String>>>> itr = hashMap.entrySet().iterator();
-
             while(itr.hasNext())
             {
                 Map.Entry<String, List<Pair<String, String>>> entry = itr.next();
                 String key = entry.getKey();
                 List<Pair<String, String>> pair = entry.getValue();
                 System.out.println("Destination: " + key);
-                System.out.println(pair);
-//                System.out.println("Stop number: " + ? + " is " + ?);
-                /**
-                 * Print pair data structure.
-                 */
+                for (Pair x : pair)
+                {
+                    System.out.println("Stop number: " + x.getKey().toString() + " is " + x.getValue().toString());
+                }
             }
-            
+
             // Asks user to lookup another destination, or to close program.
-            System.out.print("Do you want to check different destination? Please type Y to continue or press any other key to exit.");
-            decision = scanner.next();
+            System.out.print("\nDo you want to check different destination? Please type Y to continue or press any other key to exit. ");
+            decision = scanner.next().toUpperCase();
+            System.out.println();
         }
         scanner.close();
     }
--- RegularExpressions/src/RouteFinder/RouteFinder.java
+++ RegularExpressions/src/RouteFinder/RouteFinder.java
@@ -15,6 +15,7 @@ import java.util.regex.Pattern;
 
 public class RouteFinder implements IRouteFinder
 {
+    // Passes the HTML url to scrape source code.
     @Override
     public String getUrlText(String url) throws IOException
     {
@@ -22,30 +23,38 @@ public class RouteFinder implements IRouteFinder
         return text;
     }
 
+    // Collects routes for user specified locations. Returns a hashmap of routes and URL's attached to routes.
     @Override
     public Map<String, String> getBusRoutesUrls(String routesUrlText, char destInitial)
     {
-        Map<String, String> hashMap = new HashMap<>();
+        Map<String, String> hashMap = new HashMap<>(); // Create Map
         String regexLocationName = "<h3>(" + destInitial + ".*)</h3>";
         Pattern pattern1 = Pattern.compile(regexLocationName);
         Matcher matcher1 = pattern1.matcher(routesUrlText);
+
+        // Locates the specified location by user character input.
         while(matcher1.find())
         {
-            String location = matcher1.group(1);
+            String location = matcher1.group(1); // Stores location name.
             System.out.println(location); // Prints locations
             String regexLocationRoutes = "<h3>\\Q" + location + "\\E</h3>[\\w[\\W]]+?(?:<hr id=\"(?:.*)\" />)";
             Pattern pattern2 = Pattern.compile(regexLocationRoutes);
             Matcher matcher2 = pattern2.matcher(routesUrlText);
+
+            // Scrapes the route numbers for location.
             while(matcher2.find())
             {
                 String regexRoutes = "(?:success>|\">)(.*)</a></strong>";
                 String routesText = matcher2.group(0);
                 Pattern pattern3 = Pattern.compile(regexRoutes);
                 Matcher matcher3 = pattern3.matcher(routesText);
+
+                // Appends route number to url.
                 while(matcher3.find())
                 {
                     System.out.println(matcher3.group(1)); // Prints location respective routes
-                    String routeURL = "https://www.communitytransit.org/busservice/schedules/route/" + matcher3.group(1);
+                    String routeNUmber = matcher3.group(1);
+                    String routeURL = "https://www.communitytransit.org/busservice/schedules/route/" + routeNUmber + "\n";
                     hashMap.put(matcher3.group(1), routeURL); // Stores location route and route url.
                 }
                 System.out.println("++++++++++++++++++++++++++++++++++++");
@@ -57,42 +66,45 @@ public class RouteFinder implements IRouteFinder
     @Override
     public Map<String, List<Pair<String, String>>> getRouteStops(Map<String, String> busRoutesUrls, String routeId) throws IOException
     {
-        Pair<String, String> pair;
-        List<Pair<String, String>> list = new ArrayList<>();
-        Map<String, List<Pair<String, String>>> hashMap = new HashMap<>();
-
+        Map<String, List<Pair<String, String>>> hashMap = new HashMap<>(); // Create hashmap.
         String routeUrl = busRoutesUrls.get(routeId);
-        System.out.println("The URl for your route is: " + routeUrl);
+        System.out.println("\nThe URl for your route is: " + routeUrl);
         String text = collectHTMLText(routeUrl);
         String regexDestinationName = "<h2>Weekday<small>(.*)</small></h2>";
         Pattern pattern1 = Pattern.compile(regexDestinationName);
         Matcher matcher1 = pattern1.matcher(text);
-        int stopNumber = 0;
+
+        // Regex is used here to scrape the html code for weekday only.
         while(matcher1.find())
         {
             String destination = matcher1.group(1);
-//            System.out.println("Destination: " + destination);
             String regexStop = "<h2>Weekday<small>" + destination + "</small>[\\w[\\W]]+?</thead>";
             Pattern pattern2 = Pattern.compile(regexStop);
             Matcher matcher2 = pattern2.matcher(text);
+
+            // Regex is used here to scrapes the stop numbers and location names.
             while(matcher2.find())
             {
                 String regexStopName = "<strong class=\"fa fa-stack-1x\">(.*)</strong>[\\w[\\W]]+?<p>(.*)</p>";
                 String routesText = matcher2.group(0);
                 Pattern pattern3 = Pattern.compile(regexStopName);
                 Matcher matcher3 = pattern3.matcher(routesText);
+                List<Pair<String, String>> list = new ArrayList<>(); // Create list.
+
+                // Stores the stops numbers and location names as a pair into a list.
                 while(matcher3.find())
                 {
-//                    System.out.println("Stop number: " + matcher3.group(1) + " is " + matcher3.group(2));
-                    Pair p1 = new Pair(matcher3.group(1), matcher3.group(2));
-                    list.add(p1);
-                    hashMap.put(destination, list);
+                    Pair pairStopAndLocation = new Pair(matcher3.group(1), matcher3.group(2)); // Create pair.
+                    list.add(pairStopAndLocation);
                 }
+                // Stores the list into a hashmap.
+                hashMap.put(destination, list);
             }
         }
         return hashMap;
     }
 
+    // Scrapes HTML source code.
     public String collectHTMLText(String url) throws IOException
     {
         URLConnection communityTransit = new URL(url).openConnection();
