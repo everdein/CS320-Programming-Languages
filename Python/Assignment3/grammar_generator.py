f0be9acc3cbec4c42f8290c92539a00a7b491807
Matthew Clark <Everdein@gmail.com>
2020-03-12 22:57:55 -0700

    Assignment 3
    Cleaned code.

--- Python/Assignment3/grammar_generator.py
+++ Python/Assignment3/grammar_generator.py
@@ -4,34 +4,19 @@ class GrammarGenerator:
         # print("What is the file name?")
         # file_name = input()
         file_name = "simple.txt"
+        # file_name = "sentence.txt"
         g.read_file(file_name)
-
-    # @staticmethod
-    # def read_file(file_name):
-    #     d1 = {}
-    #     with open(file_name) as file:
-    #         for line in file:
-    #             (key, val) = line.split('::=')
-    #             d1[str(key)] = val.strip()
-    #     g.get_user_input(d1)
 
     @staticmethod
     def read_file(file_name):
         d1 = {}
-        count = 0
         with open(file_name) as file:
             for line in file:
                 (key, val) = line.split('::=')
                 d1[str(key)] = {}
                 strippedVal = val.strip()
                 for stripped in strippedVal.split('|'):
-                    # Generate new dictionary per split
-                    # d1[str(key)][str(key)] = stripped
                     d1[str(key)][stripped] = stripped
-                    # count = count + 1
-                    # for val in d1.values():
-                    #     print(val)
-
         print(d1)
         for val in d1.values():
             print(val)
@@ -42,9 +27,14 @@ class GrammarGenerator:
         print("Available symbols to generate are:")
         print(list(d1.keys()))
         print("What do you want to generate (Enter to quit) ?")
-        # generate = input()
+        generate = input()
         print("How many do you want me to generate?")
-        # num = input()
+        num = input()
+        g.recursion_formula(generate, num)
+
+    @staticmethod
+    def recursion_formula(generate, num):
+        print(generate, num)
 
 
 if __name__ == '__main__':
